b45f5fc561e6df37c36d175a5efedd79a69c0ad8
Gabriel Barbosa <gabrieldecarvalhu@gmail.com>
2025-06-23 11:50:51 -0300

    add: lenis

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
+import { useRef } from "react"
 import NavBar from "./Components/NavBar"
 import Home from "./Pages/Home"
+import { useGSAP } from '@gsap/react'
+import { ReactLenis } from 'lenis/react'
+import gsap from 'gsap'
 
 function App() {
+  const lenisRef = useRef()
+
+  useGSAP(() => {
+    function update(time) {
+      lenisRef.current?.lenis?.raf(time * 1000)
+    }
+
+    gsap.ticker.add(update)
+
+    // return () => gsap.ticker.remove(update)
+  }, [])
 
   return (
-   <>
-   <NavBar/>
-   <Home/>
-   
-   </>
+    <ReactLenis root options={{autoRaf:false, smoothWheel: true}} ref={lenisRef}>
+
+      <NavBar />
+      <Home />
+
+    </ReactLenis>
+
+
+
   )
 }
 

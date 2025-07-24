da898c56b6b4b6a2a3890767a054db4f311d265a
Gabriel Barbosa <gabrieldecarvalhu@gmail.com>
2025-07-24 16:03:26 -0300

    change logic mobile

--- src/Pages/About/AboutComponents/ICanHelp/index.jsx
+++ src/Pages/About/AboutComponents/ICanHelp/index.jsx
@@ -6,12 +6,20 @@ import { useRef } from "react";
 export default function ICanHelp() {
     gsap.registerPlugin(useGSAP, ScrollTrigger);
     const titleRef = useRef()
+    const listRef = useRef([])
+
+    const list = [
+        {id: 1, job: "Sites", desc: <>Eu crio sites responsivos, com foco em pequenas <br></br> interaçoes, utilizando react, gsap e three.js</>},
+        {id: 2, job: "Sistemas", desc: <>Já trabalhei em diversos sitemas front-end, desde <br></br> grandes projetos a pequenos SASS</>},
+        {id: 3, job: "Design", desc: <>Caso nao tenha um projeto pronto, podemos<br></br> criar algo com sua cara</>}
+    ]
 
 
     useGSAP(() => {
         const sections = [...document.querySelectorAll('.item-list')];
+        console.log(listRef.current)
 
-        sections.forEach((sec) => {
+        listRef.current.forEach((sec) => {
             ScrollTrigger.create({
                 trigger: sec,
                 start: "top center",
@@ -47,14 +55,17 @@ export default function ICanHelp() {
                 <h1 ref={titleRef}>Eu posso te ajudar com...</h1>
 
                 <div className='list-help'>
-                    <div className='item-list'>
-                        <h2>Sites</h2>
+
+                   {list.map((item,index) => (
+                     <div className='item-list' key={item.id} ref={(el) => listRef.current[index] = el}>
+                        <h2>{item.job}</h2>
                         <div className='text-grid-help'>
-                            <p>Eu crio sites responsivos, com foco em pequenas <br></br> interaçoes, utilizando react, gsap e three.js</p>
+                            <p>{item.desc}</p>
                         </div>
                     </div>
+                   ))}
 
-                    <div className='item-list'>
+                    {/* <div className='item-list'>
                         <h2>Sistemas</h2>
                         <div className='text-grid-help'>
                             <p>Já trabalhei em diversos sitemas front-end, desde <br></br> grandes projetos a pequenos SASS</p>
@@ -66,7 +77,7 @@ export default function ICanHelp() {
                         <div className='text-grid-help'>
                             <p>Caso nao tenha um projeto pronto, podemos<br></br> criar algo com sua cara</p>
                         </div>
-                    </div>
+                    </div> */}
 
                 </div>
 
